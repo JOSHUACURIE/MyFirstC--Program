85b7f46775eb9c1fa359256ae9f6849bf9a8c0a7
JOSHUACURIE <joshuajuma039@gmail.com>
2026-02-18 22:16:54 +0300

    learning taking user inputs

---
+++ Input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   // int age;
+    //cin>>age;
+
+   // cout<<age;
+
+string fullName;
+cout<<"Enter your Full Name: \n";
+getline(cin,fullName);
+cout<<"Full Name: "+fullName;
+
+
+     
+}
