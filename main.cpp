668d7aab72c558d9ce784cf163545909ab7aa735
JOSHUACURIE <joshuajuma039@gmail.com>
2026-02-18 22:02:53 +0300

    my first cpp program

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=3;
+    int y=5;
+    int sum=x + 5;
+    if(x>y){
+        std::cout<<"3 is greater than 5";
+    } else{
+        std::cout<<"5 is greater than 3\n";
+    }
+    std::cout<<"My First C++ Program\n";
+    std::cout<<sum;
+    return 0;
+
+}
